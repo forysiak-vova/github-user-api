d3f4bbafe2202745b6261282e76195dc21c07bae
forysiak-vova <volodja.forus@gmail.com>
2022-10-09 16:32:05 +0200

    fix style and user location

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ const getUserLocalStorage = () => {
   gallery.innerHTML = `
   <div><img src="${user.avatar_url}" width="200"> <div>
         <h1>${user.login}</h1>
-          <h2>${user.location}</h2>
+          <h2>${user.location ? user.location : 'no location'}</h2>
         `;
 };
 window.onload = () => {
